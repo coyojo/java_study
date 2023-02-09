6f1bb87ec0781a7ecec8bf82f6f91f3338509bb9
coyojo10 <coyojo@naver.com>
2023-02-09 16:34:08 +0900

    ch9 _ toString

---
+++ java_study/src/ch9/Ex9_3.java
@@ -0,0 +1,58 @@
+package ch9;
+
+import java.util.Objects;
+
+class Card {
+	String kind;
+	int number;
+	
+	Card() {
+		this("SPADE",1);
+		
+	}
+	
+	Card(String kind, int number) {
+		this.kind = kind;
+		this.number = number;
+	}
+	
+	//Object클래스의 toString()을 오버라이딩 , 오버라이딩 안해주면 주소값이 출력된다
+		public String toString() {
+			return "kind : "+kind+", number : " + number; 
+		}
+	
+		
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Card))
+			return false;
+		Card c = (Card)obj;
+		return this.kind.equals(c.kind) && this.number == c.number;
+	}
+	
+	//equals()를 오버라이딩하면 hashCode()도 오버라이딩 해야한다.
+	public int hashCode() {
+		return Objects.hash(kind,number);
+	}
+		
+}
+
+
+
+public class Ex9_3 {
+
+	public static void main(String[] args) {
+	 // 
+		Card c1 = new Card();
+		Card c2 = new Card();
+		
+		
+		System.out.println(c1.equals(c2));
+		System.out.println(c1.toString());  
+		System.out.println(c2.toString());
+		
+		System.out.println(c1.hashCode());  
+		System.out.println(c2.hashCode());
+
+	}
+
+}
